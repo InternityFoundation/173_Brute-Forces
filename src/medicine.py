d45332fa809f947105988dd613f0be1af3ce3fd7
shreyanshvarshney <52509486+shreyanshvarshney@users.noreply.github.com>
2019-11-09 23:14:55 +0530

    added updated medicine.py and medicineChance.py

--- src/medicine.py
+++ src/medicine.py
@@ -1,24 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+# THIS SCRIPT ADDS THE LIST OF SALT OF ANY PARTICULAR MEDICINE
+# AND PRINT THE LIST AS WELL
+# ALSO UPDATES THE SAME TO FIREBASE
 import requests
+import sys
+import firebaseConfig
 from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
-
-
-# In[2]:
-
-
+# import pandas as pd
+# import numpy as np
 html = requests.get("https://www.1mg.com/drugs/pan-d-capsule-343042")
-
-
-# In[3]:
-
-
 salt_class = "saltInfo DrugHeader__meta-value___vqYM0"
 drug_name_class = "DrugHeader__title___1NKLq"
 best_price_class = "DrugPriceBox__best-price___32JXw"
@@ -26,10 +15,6 @@ prod_class = "style__horizontal-card___1Zwmt"
 search = "https://www.1mg.com/search/all?name="
 base = "https://www.1mg.com"
 salt_class_new = "FactBox__rowContent__JHFU7 FactBox__flexCenter__zYkHn col-3"
-
-
-# In[4]:
-
 
 def getMedicineInfo(med_name):
     med_name = med_name.strip().split(" ")
@@ -39,14 +24,14 @@ def getMedicineInfo(med_name):
     medname = medname[:-1]
     #print(medname)
     html = requests.get(search+medname)
-    data = BeautifulSoup(html.text)
+    data = BeautifulSoup(html.text,"html.parser")
     a = data.find(class_=prod_class)
     if a == None:
         return ["NOT FOUND"]
     add = a.a['href']
     html1 = requests.get(base+add)
 #     print(html1)
-    data1 = BeautifulSoup(html1.text)
+    data1 = BeautifulSoup(html1.text,"html.parser")
     #         drug = data1.find(class_ =drug_name_class)
     #         price = data1.find(class_ =best_price_class)
     salt = data1.find(class_ = salt_class_new)
@@ -63,10 +48,15 @@ def getMedicineInfo(med_name):
             ans.append(component[:index].strip())
     return ans
 
-
-# In[5]:
-
-
-saltName = input()
-print(getMedicineInfo(saltName))
-
+if __name__ == "__main__":
+    medName = open("input.txt","r").read()
+    saltNameList = getMedicineInfo(medName)
+    print(saltNameList)
+    #FLUSH!!!!
+    # sys.stdout.flush
+    if saltNameList != ["NOT FOUND"]:
+        firebaseConfig.pushToFirebase(medName,saltNameList,"dummy/medicine")
+    # print(firebaseConfig.getMedicineAllergyChance(["salt1"]))
+    # saltList, saltfreq = firebaseConfig.getDataFromFirebase("dummy/medicine")
+    # print(saltList)
+    # print(saltfreq)
