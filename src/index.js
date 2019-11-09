b3fe2587292fb892fd8382e59b972c729f519fcd
shreyanshvarshney <52509486+shreyanshvarshney@users.noreply.github.com>
2019-11-10 00:09:04 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,61 @@ const express = require('express');
 const bodyParser= require('body-parser');
 const fs = require('fs');
 
+
+//Firebase Code Begins.
+var firebase = require("firebase");
+var config = require('./myconfig.json');
+
+require("firebase/auth");
+require("firebase/firestore");
+
+firebase.initializeApp(config);
+
+var db = firebase.database();
+
+//Express Code.
 const app = express();
 app.use(bodyParser.json());
 
+
+//CRUD FOR MEDICINES.
+app.post('/add-medicine', (req, res) => {
+    //Python.
+    res.send("Medicine added");
+});
+
+app.delete('/delete', (req, res) => {
+    console.log("Item Deleted.");
+});
+
+app.get('/profile', (req, res) => {
+    var arr = [];
+    db.ref("dummy/medicine").on('value', (snapshot) =>{
+        //snapshot.val();
+        snapshot.forEach(item=> {
+            
+            var temp = {
+                med_name: item.val().med_name,
+                salts: item.val().salt
+            }
+            arr.push(temp);
+            //console.log(temp);
+        });
+        
+        console.log(arr);
+
+        //Send all the database to front end.
+        res.send(arr);
+        
+        //res.send("Home Page" + arr);    
+        //return arr;
+    });
+});
+
+
+//BASIC FRONT-END.
 app.get('/', (req, res) => {
-    res.send("Home Page");
+    
 });
 
 app.get('/medicine', (req, res) => {
@@ -17,10 +67,8 @@ app.get('/food', (req, res) => {
     res.send("FoodItems");
 });
 
-app.post('/add-medicine', (req, res) => {
-    res.send("Medicine added");
-});
 
+//ALLERGEY TEST.
 app.post('/food-allergey', (req, res) => {
     
 });
@@ -49,6 +97,8 @@ app.post('/medicine-allergey', (req, res) => {
     });
 });
 
+
+//EXPRESS SERVER.
 const port = 5000;
 app.listen(port,() => {
     console.log("Server Started on " + port);
